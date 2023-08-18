0b184e5eccae2eb6cdfeb11e85a6f80f42b76203
Sara Lahrach <slahrach@student.1337.ma>
2023-08-18 20:33:08 +0100

    fix mariadb

--- srcs/requirements/mariadb/create.sh
+++ srcs/requirements/mariadb/create.sh
@@ -6,12 +6,23 @@ fi
 if [ ! -d /run/mysqld ]; then
  mkdir /run/mysqld
 fi
-if [ ! -d /var/log/mysql/ ]; then
- mkdir /var/log/mysql/ && touch /var/log/mysql/error.log && chown -R mysql:mysql /var/log/mysql
+
+if [ ! -d /var/lib/mysql/mysql ]; then
+ mysql_install_db --user=root
 fi
 
-service mariadb start
-sleep 5
-mysql < /mariadb-conf.d/init.sql
+if [ ! -d /var/log/mysql/ ]; then
+ mkdir /var/log/mysql/ && chown -R root:root /var/log/mysql && touch /var/log/mysql/error.log
+fi
 
-exec mysqld
+echo "CREATE DATABASE $DB_NAME;" >> init.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> init.sql
+echo "FLUSH PRIVILEGES;" >> init.sql
+
+
+mysqld -u root &
+sleep 5
+mysql --user=root < init.sql
+
+# Keep the container running
+tail -f /dev/null
--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
-#downloads the WordPress core files, the option ( --allow-root ) will run the command as root
 #creates new user with editor role
-wp user create ${WP_USER} --user_pass=${WP_PASSWORD} --role=editor --allow-root
-#generates the WordPress configuration file
-wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost="mariadb" --allow-root
 
-# wp user create ${WP_USER} --user_pass=${WP_PASSWORD} --role=editor --allow-root
-#install a new WordPress site using the downloaded core files
-#--url specifies the URL of your website, --title is the site title, --admin_user, --admin_password, and --admin_email set up the initial administrator account for the site.
+# if [ ! -d "/var/www/html/" ]; then
+#     mkdir -p /var/www/html/
+# fi
+
+cd /var/www/html/
+
+cat wp-config.php
+#change the wp-config.php file to use the values passed in as environment variables
+sed -i "s/username_here/${DB_USER}/" wp-config.php
+sed -i "s/password_here/${DB_PASSWORD}/" wp-config.php
+sed -i "s/database_name_here/${DB_NAME}/" wp-config.php
+
+wp core download --allow-root
+# wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost="mariadb" --allow-root
+
+wp user create ${WP_USER} --user_pass=${WP_PASSWORD} --role=editor --allow-root
+
 wp core install --url=http://localhost --title="My WordPress Site" --admin_user=${ADMIN_USER} --admin_password=${ADMIN_PASSWORD} --admin_email=${ADMIN_EMAIL} --allow-root
 
 
