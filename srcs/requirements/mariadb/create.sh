ac281ecd940b17369bf1715179e68976acb6132b
Sara Lahrach <slahrach@student.1337.ma>
2023-08-21 20:40:40 +0100

    remove init.sql and execute directly

--- srcs/requirements/mariadb/create.sh
+++ srcs/requirements/mariadb/create.sh
@@ -1,34 +1,53 @@
 #!/bin/bash
 
-if [ ! -d /var/run/mysqld ]; then
- mkdir /var/run/mysqld
-fi
-if [ ! -d /run/mysqld ]; then
- mkdir /run/mysqld
-fi
+service mysql start
 
-if [ ! -d /var/lib/mysql/mysql ]; then
- mysql_install_db --user=root
-fi
+sleep 3
 
-if [ ! -d /var/log/mysql/ ]; then
- mkdir /var/log/mysql/ && chown -R root:root /var/log/mysql && touch /var/log/mysql/error.log
-fi
+mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
 
-echo "CREATE DATABASE $DB_NAME;" >> init.sql
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> init.sql
-echo "FLUSH PRIVILEGES;" >> init.sql
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+
+mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* to '$DB_USER'@'%';"
+
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+service mysql stop
+
+mysqld_safe
 
 
-service mariadb start
-# Loop for checking if the mariadb server is up or not
-while ! mysqladmin ping -hlocalhost --silent 2>/dev/null; do
- sleep 1
-done
-mysql -u root < init.sql
-service mariadb stop
-while  mysqladmin ping -hlocalhost --silent 2>/dev/null; do
- sleep 1
-done
-# run the service in the foreground
-exec mysqld -u root
+
+# #!/bin/bash
+
+# if [ ! -d /var/run/mysqld ]; then
+#  mkdir /var/run/mysqld
+# fi
+# if [ ! -d /run/mysqld ]; then
+#  mkdir /run/mysqld
+# fi
+
+# if [ ! -d /var/lib/mysql/mysql ]; then
+#  mysql_install_db --user=root
+# fi
+
+# if [ ! -d /var/log/mysql/ ]; then
+#  mkdir /var/log/mysql/ && chown -R root:root /var/log/mysql && touch /var/log/mysql/error.log
+# fi
+
+# service mysql start && sleep 5
+
+# # Create a new database
+# mysql -e "CREATE DATABASE ${DB_NAME};"
+
+# # Create a new user and grant permissions
+# mysql -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+# mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+# mysql -u root --password=${DB_ROOT_PASSWORD} -e "use mysql; update user set password = password('${DB_ROOT_PASSWORD}') , plugin = '' where user = 'root';"
+# # Flush the privileges
+# mysql -e "FLUSH PRIVILEGES;"
+
+# service mysql stop
+
+
+# mysqld_safe
--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -7,8 +7,6 @@ fi
 
 cd /var/www/html/
 
-echo "Hello World"
-
 # cat wp-config.php
 #change the wp-config.php file to use the values passed in as environment variables
 # sed -i "s/username_here/${DB_USER}/" wp-config.php
@@ -18,19 +16,15 @@ echo "Hello World"
 #create the config file
 wp core download --path=/var/www/html/ --allow-root
 
-echo "hey"
 
 wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost="mariadb" --allow-root
 
-echo "wow"
 
 # wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost="mariadb" --allow-root
 
-echo "no"
 
 wp core install --url=https://localhost --title="My WordPress Site" --admin_user=${ADMIN_USER} --admin_password=${ADMIN_PASSWORD} --admin_email=${ADMIN_EMAIL} --allow-root
 
-echo "ouiii"
 # wp user create ${WP_USER} ${ADMIN_EMAIL} --user_pass=${WP_PASSWORD} --role=editor --allow-root
 
 exec php-fpm7.3 -F -R
